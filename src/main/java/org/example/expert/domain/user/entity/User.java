ba35838c76b8335ef9646bf7d5e5ee716ce29df5
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-06-19 09:16:18 +0900

    feat: 2. 코드 추가 퀴즈 - JWT의 이해2

--- src/main/java/org/example/expert/domain/user/entity/User.java
+++ src/main/java/org/example/expert/domain/user/entity/User.java
@@ -22,7 +22,7 @@ public class User extends Timestamped {
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
-    public User(String email, String password, UserRole userRole) {
+    public User(String email, String password, UserRole userRole, String nickname) {
         this.email = email;
         this.password = password;
         this.userRole = userRole;
